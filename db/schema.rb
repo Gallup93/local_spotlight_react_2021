f310f6d84a3817b157ae6104d63d3a130dac9dfc
Michael Gallup <Gallup93@icloud.com>
2021-02-02 16:55:27 -0500

    Run migration, add seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_194859) do
+ActiveRecord::Schema.define(version: 2021_02_02_213903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artists", force: :cascade do |t|
+    t.string "city"
+    t.string "state"
+    t.string "spotify_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,9 @@
 2.times do |i|
     User.create(username: "TestUser1", email: "user-#{i+1}@example.com", password: "password", password_confirmation: "password")
 end
+
+city_state = { "Rockford" => "IL", "Rockford" => "IL", "Chicago" => "IL", "Chicago" => "IL", "Denver" => "CO"}
+spotify_ids = ["1ZOtGD23o1UK8XQDA9D2cc", "5PxhTwi9zUw26P4UUBuRs6", "2vnB6tuQMaQpORiRdvXF9H", "1SoBNpuC0N4nvaQFENS0qf", "5F24VT2NBrbVTIAOmzWBux"]
+5.times do |i|
+  Artist.create(city: "#{city_state["#{city_state.keys[0]}"]}", state: "#{city_state["#{city_state.values[0]}"]}", spotify_id: "#{spotify_ids[i]}")
+end
