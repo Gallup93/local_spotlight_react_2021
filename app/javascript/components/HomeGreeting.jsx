eac9d6134b22609ce1dcdea0b13d51780127bf9b
Michael Gallup <Gallup93@icloud.com>
2021-02-03 14:44:23 -0500

    Add div class to rendered HomeGreeting component

--- app/javascript/components/HomeGreeting.jsx
+++ app/javascript/components/HomeGreeting.jsx
@@ -4,9 +4,10 @@ import PropTypes from "prop-types";
 class HomeGreeting extends React.Component {
   render () {
     return (
-      <div>
+      <div class="home-container">
         <h4>Hello {this.props.name}, </h4>
         <h2>Weclome to Local Spotlight!</h2>
+        <p>this is a public service announcement; this is only a test</p>
       </div>
     );
   }
