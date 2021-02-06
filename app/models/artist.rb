9e7296b55f74b93bfb85171a451aa4e6cb6bd779
Michael Gallup <Gallup93@icloud.com>
2021-02-06 14:23:43 -0500

    Add Artist model validations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,8 @@
 class Artist < ApplicationRecord
+  validates :spotify_id, presence: true, uniqueness: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :name, presence: true
+  validates :followers, presence: true
+  validates :popularity, presence: true
 end
