4c33e8f1aa711c2aca8c267f5a756c0d7c364130
Michael Gallup <Gallup93@icloud.com>
2021-02-06 20:57:41 -0500

    Refactor devise routes for user to use custom controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :locations
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations'}
   root 'pages#home'
   resources :artists
 
