5969cfb2fa84038bfafe789c04aa6c5a712291d1
Michael Gallup <Gallup93@icloud.com>
2021-02-08 21:17:06 -0500

    Refactor ArtistStitcher to return JSON and not save new objects

--- lib/artist_stitcher.rb
+++ lib/artist_stitcher.rb
@@ -1,20 +1,23 @@
 class ArtistStitcher
   require 'spotify_api_helper'
+  require 'json'
+
+  attr_reader :artist
 
   def initialize(artist)
-    return stitch_artist(artist)
+    @artist = artist
+    stitch_artist(artist)
   end
 
   def stitch_artist(artist)
     spotify_object = SpotifyApiHelper.new.get_artist_object(artist.spotify_id)
     if spotify_object
-      artist.name = spotify_object["name"]
-      artist.followers = spotify_object["followers"]["total"]
-      artist.genres = spotify_object["genres"]
-      artist.popularity = spotify_object["popularity"]
-      artist.images = spotify_object["images"].map{|image| image["url"]}
-
-      artist.save
+      @artist.name = spotify_object["name"]
+      @artist.followers = spotify_object["followers"]["total"]
+      @artist.genres = spotify_object["genres"]
+      @artist.popularity = spotify_object["popularity"]
+      @artist.images = spotify_object["images"].map{|image| image["url"]}
+      @artist = @artist.to_json
     else
       puts "inalid spotify id"
       false
