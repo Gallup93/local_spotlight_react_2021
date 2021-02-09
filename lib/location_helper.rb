1c00b3ae90ebe1fbf2c6feeb2fe7a629670d173d
Michael Gallup <Gallup93@icloud.com>
2021-02-09 14:16:37 -0500

    Add city/state verification method to LocationHelper

--- lib/location_helper.rb
+++ lib/location_helper.rb
@@ -2,4 +2,23 @@ class LocationHelper
   def self.find_by_city_state(location)
     Location.where("city = ? and state = ?", "#{location[:city]}", "#{location[:state]}" )
   end
+  def self.validate_city_state(location)
+    state_abrv = location[:state].to_sym.upcase
+    all_states = CS.get(:us)
+    state_name = all_states[state_abrv]
+
+    if state_name
+      cities = CS.get(:us, state_abrv)
+      if cities.find {|city| city.downcase == location[:city].downcase}
+        true
+      else
+        false
+      end
+    else
+      return false
+    end
+  end
+
+  private
+
 end
