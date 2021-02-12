982b7f7dd10d9ba34c7125374b70e11806067956
Michael Gallup <Gallup93@icloud.com>
2021-02-12 18:31:11 -0500

    Refactor location_helper_spec to be more thourough

--- spec/lib/location_helper_spec.rb
+++ spec/lib/location_helper_spec.rb
@@ -3,19 +3,73 @@ require './lib/location_helper'
 
 RSpec.describe "Location Helper" do
 
-  it "finds existing location in DB by city/state" do
-    location = { city: "Denver", state: "CO"}
+  describe "get_existing()" do
+    it "gets existing location from db" do
+      city_state = { city: "rockford", state: "IL"}
+      existing = LocationHelper.get_existing(city_state)
+      expect(existing.city).to eq("rockford")
+    end
 
-    found_location = LocationHelper.find_by_city_state(location)
-
-    expect(found_location[0].city).to eq("Denver")
+    it "returns nil for non existing location" do
+      city_state = { city: "reno", state: "NV"}
+      existing = LocationHelper.get_existing(city_state)
+      expect(existing).to eq(nil)
+    end
   end
 
-  it "verifies valid location by city/state" do
-    location = { city: "Rockford", state: "IL"}
+  describe "check_if_exists()" do
+    it "returns true if existing" do
+      city_state = { city: "rockford", state: "IL"}
+      result = LocationHelper.check_if_exists(city_state)
+      expect(result).to eq(true)
+    end
 
-    result = LocationHelper.validate_city_state(location)
+    it "returns false if non-existing" do
+      city_state = { city: "reno", state: "NV"}
+      result = LocationHelper.check_if_exists(city_state)
+      expect(result).to eq(false)
+    end
+  end
 
-    expect(result).to eq(true)
+  describe "verify_state()" do
+    it "returns true if valid" do
+      state = :MA
+      result = LocationHelper.verify_state(state)
+      expect(result).to eq(true)
+    end
+
+    it "returns false if invalid" do
+      state = :MG
+      result = LocationHelper.verify_state(state)
+      expect(result).to eq(false)
+    end
+  end
+
+  describe "verify_city()" do
+    it "returns true if valid" do
+      city_state = { city: "framingham", state: "MA" }
+      result = LocationHelper.verify_city(city_state)
+      expect(result).to eq(true)
+    end
+
+    it "returns false if invalid" do
+      city_state = { city: "framingFord", state: "MA" }
+      result = LocationHelper.verify_city(city_state)
+      expect(result).to eq(false)
+    end
+  end
+
+  describe "create_location()" do
+    it "returns new lcoation_id if valid" do
+      city_state = {city: "Beloit", state: "WI"}
+      result = LocationHelper.create_location(city_state)
+      expect(result).to eq(Location.last.id)
+    end
+
+    it "returns error messahe if invalid" do
+      city_state = {city: "", state: "WI"}
+      result = LocationHelper.create_location(city_state)
+      expect(result).to eq(nil)
+    end
   end
 end
