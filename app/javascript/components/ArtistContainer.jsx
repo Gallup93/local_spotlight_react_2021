b05cb5ad0cf62b411ffddb76968a2c7d8a1e18ed
Michael Gallup <Gallup93@icloud.com>
2021-02-20 18:38:20 -0500

    Add more stylizing and cleanup to ArtistIndex page

--- app/javascript/components/ArtistContainer.jsx
+++ app/javascript/components/ArtistContainer.jsx
@@ -7,11 +7,10 @@ class ArtistContainer extends React.Component {
     this.state = { artist: this.props.artist};
   }
 
-
   render(){
     return(
       <div class="artist-container" key={`artist-${this.state.artist.id}`}>
-        <td>{this.state.artist.name} | CITY: {this.state.artist.city}, STATE: {this.state.artist.state}, FOLLOWERS: {this.state.artist.followers}</td>
+        <td>{this.state.artist.name} --- [Followers: {this.state.artist.followers}, Popularity Rating: {this.state.artist.popularity}]</td>
       </div>
     )
   }
